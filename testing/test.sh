0f3ec3765509f30f36992cac64e48031d87b54dd
Tom <ionfury@gmail.com>
2023-05-31 11:19:14 -0400

    trying different versions of flux

--- testing/test.sh
+++ testing/test.sh
@@ -2,17 +2,17 @@
 kind --version
 # kind version 0.19.0
 
-kind create cluster --name testing --image kindest/node:v1.24.3
+kind create cluster --name testing --image kindest/node:v1.27.2
 
-docker pull ghcr.io/fluxcd/helm-controller:v0.32.1 && kind load docker-image --name testing ghcr.io/fluxcd/helm-controller:v0.32.1
-docker pull ghcr.io/fluxcd/image-automation-controller:v0.32.0 && kind load docker-image --name testing ghcr.io/fluxcd/image-automation-controller:v0.32.0
-docker pull ghcr.io/fluxcd/image-reflector-controller:v0.27.0 && kind load docker-image --name testing ghcr.io/fluxcd/image-reflector-controller:v0.27.0
-docker pull ghcr.io/fluxcd/kustomize-controller:v1.0.0-rc.1 && kind load docker-image --name testing ghcr.io/fluxcd/kustomize-controller:v1.0.0-rc.1
-docker pull ghcr.io/fluxcd/notification-controller:v1.0.0-rc.1 && kind load docker-image --name testing ghcr.io/fluxcd/notification-controller:v1.0.0-rc.1
-docker pull ghcr.io/fluxcd/source-controller:v1.0.0-rc.1 && kind load docker-image --name testing ghcr.io/fluxcd/source-controller:v1.0.0-rc.1
+#docker pull ghcr.io/fluxcd/helm-controller:v0.32.1 && kind load docker-image --name testing ghcr.io/fluxcd/helm-controller:v0.32.1
+#docker pull ghcr.io/fluxcd/image-automation-controller:v0.32.0 && kind load docker-image --name testing ghcr.io/fluxcd/image-automation-controller:v0.32.0
+#docker pull ghcr.io/fluxcd/image-reflector-controller:v0.27.0 && kind load docker-image --name testing ghcr.io/fluxcd/image-reflector-controller:v0.27.0
+#docker pull ghcr.io/fluxcd/kustomize-controller:v0.25.10 && kind load docker-image --name testing ghcr.io/fluxcd/kustomize-controller:v0.25.10
+#docker pull ghcr.io/fluxcd/notification-controller:v0.25.10 && kind load docker-image --name testing ghcr.io/fluxcd/notification-controller:v0.25.10
+#docker pull ghcr.io/fluxcd/source-controller:v0.25.10 && kind load docker-image --name testing ghcr.io/fluxcd/source-controller:v0.25.10
 
-kubectl apply -f testing/gotk-components.yaml
+kubectl apply -f "v0.37.0.yaml"
 
-kubectl apply -f testing/gotk-sync.yaml
+kubectl apply -f gotk-sync.yaml
 
-kubectl get ns test -o yaml
+kubectl get ns test -o yaml | grep goldilocks.fairwinds.com/enabled
