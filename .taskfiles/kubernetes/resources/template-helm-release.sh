5efdad19760ec63387880b672dd005feb9082733
Tom Nowak <ionfury@gmail.com>
2025-01-22 22:26:22 -0500

    Taskfile changes for doing kubernetes rendering

---
+++ .taskfiles/kubernetes/resources/template-helm-release.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+release=$1
+values=$2
+outdir=$3
+
+kustomize=$(kustomize build "$release" | flux envsubst)
+url=$(echo "$kustomize" | yq 'select(.kind == "HelmRepository") | .spec.url')
+oci=$(echo "$kustomize" | yq 'select(.kind == "HelmRepository") | .spec.type')
+chart=$(echo "$kustomize" | yq 'select(.kind == "HelmRelease") | .spec.chart.spec.chart')
+version=$(echo "$kustomize" | yq 'select(.kind == "HelmRelease") | .spec.chart.spec.version')
+
+if [ $oci == "oci" ]; then
+  helm template $chart $url/$chart --version $version --values $values --output-dir $outdir
+else
+  helm template $chart --repo $url --version $version --values $values --output-dir $outdir
+fi
